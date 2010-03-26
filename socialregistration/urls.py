c4164770767a111b255ea6b47933d558b3d11377
Jon Keating <jon@ubuntu.(none)>
2010-03-26 21:23:47 +0900

    Facebook connect requires the xd_receiver to be in the same subdirectory as the Connect URL

--- socialregistration/urls.py
+++ socialregistration/urls.py
@@ -25,7 +25,7 @@ if getattr(settings, 'FACEBOOK_API_KEY', None) is not None:
         url('^facebook/connect/$', 'socialregistration.views.facebook_connect',
             name='facebook_connect'),
 
-        url('^xd_receiver.htm', 'django.views.generic.simple.direct_to_template',
+        url('^facebook/connect/xd_receiver.htm', 'django.views.generic.simple.direct_to_template',
             {'template':'socialregistration/xd_receiver.html'},
             name='facebook_xd_receiver'),
     )
